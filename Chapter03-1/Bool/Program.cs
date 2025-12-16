dfe9e19b04972801122e3d2e101b380f36f8d574
Sangjun <jjuny349>
2025-12-16 16:54:59 +0900

    Ch3 3.4.4까지 학습

---
+++ Chapter03-1/Bool/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Bool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            bool b = false;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ Chapter03-1/Object/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object a = 123;
+            object b = 3.141592653589793238468643383279m;
+            object c = true;
+            object d = "Hello World";
+
+            Console.WriteLine(a);
+        }
+    }
+}

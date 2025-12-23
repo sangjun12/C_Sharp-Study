6ceafc3dc72edf189350e06072138078d76babdc
sangjun12 <101243437+sangjun12@users.noreply.github.com>
2025-12-24 07:16:19 +0900

    ch3 string 학습중

---
+++ Chapter03-1/Enum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Enum
+{
+    internal class Program
+    {
+
+        enum DialogResult
+        { YES, NO = 10, CANCEL = 9, CONFIRM, OK }
+
+        static void Main(string[] args)
+        {
+            const int a = 3;
+            Console.WriteLine("Const Example");
+            Console.WriteLine(a);
+
+
+
+
+            Console.WriteLine("Enum Example");
+            Console.WriteLine((int)DialogResult.YES);
+            Console.WriteLine((int)DialogResult.NO);
+            Console.WriteLine((int)DialogResult.CANCEL);
+            Console.WriteLine((int)DialogResult.CONFIRM);
+            Console.WriteLine((int)DialogResult.OK);
+
+        }
+    }
+   
+}
+
---
+++ Chapter03-1/Nullable/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+
+            Console.WriteLine(a.HasValue); // False
+            Console.WriteLine(a != null);   
+
+            a = 3;
+            Console.WriteLine(a.HasValue); // True
+            Console.WriteLine(a != null);
+            Console.WriteLine(a.Value);    // 3
+        }
+    }
+}

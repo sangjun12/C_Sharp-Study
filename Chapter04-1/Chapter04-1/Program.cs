6f81b477ddc17f41479b38774ff6b39840ed9fb3
Sangjun <jjuny349>
2025-12-24 13:33:07 +0900

    ch4 시작

--- Chapter04-1/Chapter04-1/Program.cs
+++ Chapter04-1/Chapter04-1/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a =  111 + 222;
+            Console.WriteLine($"a : {a}");
+
+            Console.WriteLine(a++);
+            Console.WriteLine(++a);
+
+            string result = "123" + "456";
+            Console.WriteLine(result);
+
+            Console.WriteLine($"3 > 4:{3 > 4}");
+            Console.WriteLine($"3 >= 4:{3 >= 4}");
+            Console.WriteLine($"3 < 4:{3 < 4}");
+            Console.WriteLine($"3 != 4:{3 != 4}");
+
+
         }
     }
 }

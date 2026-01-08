e2d091cdeed9c10ac961751c9976b0a389c6be67
sangjun12 <101243437+sangjun12@users.noreply.github.com>
2026-01-08 21:44:44 +0900

    Ch5

---
+++ Chapter05-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Chapter05_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("현재 기온을 입력하세요: ");
+            string input = Console.ReadLine();
+            int temperature = int.Parse(input);
+            Console.WriteLine($"현재 기온은 {temperature}도 입니다.");
+
+            if (temperature >= 30)
+            {
+                Console.WriteLine("오늘은 매우 더운 날씨입니다. 외출 시 가벼운 옷차림을 권장합니다.");
+            }
+
+            else if (temperature >= 20)
+            {
+                Console.WriteLine("따뜻한 날씨입니다. ");
+            }
+
+            else if(temperature >= 10)
+            {
+                Console.WriteLine("선선한 날씨입니다. 가벼운 겉옷을 챙기세요.");
+            }
+            else  
+            {
+                Console.WriteLine("쌀쌀한 날씨입니다. 외출 시 따뜻한 옷차림을 권장합니다.");
+            }
+            
+        }
+    }
+}
